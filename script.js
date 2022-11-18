b3447372d65e839ef08482db6dac11b07536fd31
Oleg Maizer <zippooleg3@gmail.com>
2022-11-18 20:50:46 +0200

    Initial

---
+++ script.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector('#navigation');
+
+function showNav(){
+    nav.classList.add('show');
+}
+
+function hidewNav(){
+    nav.classList.remove('show');
+}
+var currPos = window.scrollY;
+document.addEventListener('scroll', () => {
+  if (window.scrollY < currPos) {
+  //scroll up
+    hidewNav();
+  } else {
+  //scroll down
+    showNav();
+  }
+  currPos = window.scrollY;
+});
