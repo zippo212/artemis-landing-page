277e308b9d9023df14ab1b07d87dfbc0aa444420
Oleg Maizer <zippooleg3@gmail.com>
2022-11-23 21:50:04 +0200

    Final design

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const nav = document.querySelector('#navigation');
+const root_theme = document.querySelector(':root');
+
 
 function showNav(){
     nav.classList.add('show');
@@ -18,3 +20,24 @@ document.addEventListener('scroll', () => {
   }
   currPos = window.scrollY;
 });
+
+// Accordion
+function myAccord(idP,idImg) {
+  let whyPara = document.getElementById(idP);
+  let whyImg = document.getElementById(idImg);
+  whyPara.classList.toggle('show-para')
+  whyImg.classList.toggle('img-op')
+}
+
+const element = document.querySelector('.we-go');
+const element2 = document.querySelector('#mission');
+const observer = new IntersectionObserver(entries => {
+  element.classList.toggle( 'showtext', entries[0].isIntersecting );
+});
+
+observer.observe( element2 );
+
+function showInfo() {
+  root_theme.style.setProperty('--rtl', 'translateX(0)'); 
+  root_theme.style.setProperty('--ltr', 'translateX(0)'); 
+}
